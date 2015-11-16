f28feaeeec924f5f98a3885fa03a9208ce2ad7ce
Alex Beznos <beznosa@yahoo.com>
2015-11-16 20:46:31 +0200

    Revert "lib/simphi/form_builder.rb"
    This reverts commit 17c87ac7069d4958722c0ea0721bc7c3cc9a0a02.

--- lib/simphi.rb
+++ lib/simphi.rb
@@ -1,4 +1,13 @@
+require "simple_form"
+require "simphi/simphi_input"
+
 module Simphi
   class Engine < Rails::Engine
+  end
+end
+
+module SimpleForm
+  class FormBuilder
+    map_type :simphi, to: Simphi::SimphiInput
   end
 end
